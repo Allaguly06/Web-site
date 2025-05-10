1f12268cea6bb025698a5f64abff1ea6375edf76
Charyyev Allaguly <allagulycharyew@gmail.com>
2025-05-10 16:59:46 +0300

    добавил, порт и хост

--- Variable part of practice/flask_messenger/app.py
+++ Variable part of practice/flask_messenger/app.py
@@ -126,4 +126,4 @@ def init_db():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    socketio.run(app, debug=True)
+    socketio.run(app,host='0.0.0.0',port='5000', debug=True)
